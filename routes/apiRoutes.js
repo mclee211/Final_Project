a87822738a010fb62f7acb3c1d79c28f1e6f60fb
Michael Grill <Ultramar@Michaels-MacBook-Pro.local>
2019-03-09 11:40:35 -0500

    RoutesWork

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,5 +7,65 @@ module.exports = function(app){
         });
     });
 
-    
+    app.get("/api/pokemon", function(req, res){
+        db.Pokemon.findAll({}).then(function (dbPokemon){
+            res.json(dbPokemon);
+        });
+    });
+
+    app.post("/api/team")
+
+    app.post("/api/Pokemon")
+
+    app.put("/api/team", function(req, res){
+        db.Team.create({
+            name: req.body.name
+        },{
+            where:{
+                id: req.body.id
+            }
+        }).then(function(dbTeam){
+            res.json(dbTeam);
+        }).catch(function(err){
+            res.json(err);
+        });
+    });
+
+    app.put("/api/pokemon", function(req,res){
+        db.Pokemon.create({
+            pokemonName:req.body.name,
+            types:req.body.types,
+            moves:req.body.moves,
+            species:req.body.species,
+            sprites:req.body.sprites
+        },{
+            where:{
+                id:req.body.id
+            }
+        }).then(function(dbPokemon){
+            res.json(dbPokemon);
+        }).catch(function(err){
+            res.json(err);
+        });
+    });
+
+    app.delete("/api/team/:id", function(req, res){
+        db.Team.destroy({
+            where:{
+                id: req.params.id
+            }
+        }).then(function(dbTeam){
+            res.json(dbTeam)
+        });
+    });
+
+    app.delete("/api/pokemon/:id", function(req, res){
+        db.pokemon.destroy({
+            where:{
+                id: req.params.id
+            }
+        }).then(function(dbPokemon){
+            res.json(dbPokemon)
+        });
+    });
 }
