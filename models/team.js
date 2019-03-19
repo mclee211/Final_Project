81b82be4b5d0a0ee565ba61d188ff0148b6da4a8
SgtStarshine <starshine621@ymail.com>
2019-03-19 18:26:04 -0400

    mass update, game window and player controls established

--- models/team.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(sequelize, DataTypes){
-    var Team = sequelize.define("Team", {
-        name: DataTypes.STRING
-    });
-    Team.hasMany(Pokemon, {foreignKey:"pokemonName", sourceKey:"name"});
-}
---
+++ src/features/player/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import playerSprite from './playerSprite.png'
+import handleMovement from './movement'
+
+function Player(props) {
+    return (
+        <div
+            style={{
+                position: 'absolute',
+                top: props.position[1],
+                left: props.position[0],
+                backgroundImage: `url('${playerSprite}')`,
+                backgroundPosition: '0 0',
+                width: '40px',
+                height: '40px'
+            }}
+        />
+    )
+}
+
+function mapStateToProps(state) {
+    return {
+        ...state.player,
+    }
+}
+
+export default connect(mapStateToProps)(handleMovement(Player))
